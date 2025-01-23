a2285148d371857e18156493412315fa7771ba6c
Jaewoo Kang <jaewookang17@gmail.com>
2025-01-24 01:33:45 +0900

    Fixed backend package.json and added frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+    return (
+      <div>
+        <h1>Welcome to Dermodel</h1>
+        <p>Your React app is now running!</p>
+      </div>
+    );
+  }
+  
+  export default App;
